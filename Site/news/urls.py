eca0345a72b7c2fe7c9591a5b012744806235c4d
Ivan <150520050+Ivan-49@users.noreply.github.com>
2024-10-09 11:27:37 +0300

    404

--- Site/news/urls.py
+++ Site/news/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path, include
 
-from .views import article_detail, redirect_home
+from .views import article_detail, redirect_home, custom_404
 
 app_name = 'news'
 urlpatterns = [
     path('', redirect_home, name='home'),
-    path('<slug:slug>/', article_detail, name='article_detail'),
+    path('<str:slug>/', article_detail, name='article_detail'),
 ]
--- Site/news/views.py
+++ Site/news/views.py
@@ -4,31 +4,38 @@ from .models import Article
 
 
 def article_detail(request, slug):
-    instance = get_object_or_404(Article, slug=slug)
     global head 
     head = Article.objects.all()
-
-    for i in head:
-        print(i.url)
-
-    if request.path in '/en/home/':
-        img = True
-    else:
-        img = False
-
-    context = {
-        'instance':instance,
-        'head':head,
-        'img':img
-    }
     
-    print(f'instance url = {instance.url}')
-    print(f'main/{instance.url}.html')
-    try:
-        return render(request, f'main/{instance.url}.html', context)
-    except:
-        return render(request, 'main/home.html', context)
+    if slug in [i.slug for i in head]:
+        instance = get_object_or_404(Article, slug=slug)
+        
+        for i in head:
+            print(i.title)
 
+        if request.path in '/en/home/':
+            img = True
+        else:
+            img = False
+
+        context = {
+            'instance':instance,
+            'head':head,
+            'img':img
+        }
+        
+        print(f'instance url = {instance.url}')
+        print(f'main/{instance.url}.html')
+        
+        try:
+            return render(request, f'main/{instance.url}.html', context)
+        except:
+            return render(request, 'main/home.html', context)
+    else:
+        return redirect('/about/')
 
 def redirect_home(request):
+    return redirect('/home/')
+
+def custom_404(request, string):
     return redirect('/home/')
